142c9f33b9e252d2bab342c417b208f6bdca81f9
German Rodriguez <gerdepo04mail.com>
2026-02-18 10:59:43 -0300

    finalizacion de practica personajeDeRol from net

--- PracticaFreeCodeCampPY/practica/personajeDeRol.py
+++ PracticaFreeCodeCampPY/practica/personajeDeRol.py
@@ -1,18 +1,29 @@
+full_dot = '●'
+empty_dot = '○'
+
 def create_character(name,fuerza, inteligencia, carisma):
     if not isinstance(name,str):
-        print('The character name should be a string')
+        return 'The character name should be a string'
     if not name:
-        print('The character name should not contain spaces')
+        return'The character should have a name'
     if len(name) > 10 :
-        print('The character anme is too lang')
+        return 'The character name is too long'
     if ' ' in name :
-        print('The character name should not contain spaces')
+        return 'The character name should not contain spaces'
     if not isinstance(fuerza,int) or not isinstance(inteligencia,int) or not isinstance(carisma,int):
-        print('All stats should be integer')
+        return 'All stats should be integers'
     if fuerza < 1 or inteligencia < 1 or carisma < 1:
-        print('All stats should be no less than 1')
+        return 'All stats should be no less than 1'
     if fuerza > 4 or inteligencia > 4 or carisma > 4:
-        print('Alls stats should be no more than 4')
+        return 'All stats should be no more than 4'
     if (fuerza + inteligencia + carisma) != 7:
-        print('The character should start with 7 points')
-        return f'{name} , {fuerza} , {inteligencia} , {carisma}'
+        return 'The character should start with 7 points'
+    
+    str_line = f'STR {full_dot * fuerza}{empty_dot *(10 - fuerza)}'
+    int_line = f'INT {full_dot * inteligencia}{empty_dot * (10-inteligencia)}'
+    char_line = f'CHA {full_dot * carisma}{empty_dot * (10-carisma)}'
+
+    return f"{name}\n{str_line}\n{int_line}\n{char_line}"
+
+    
+create_character('ren',4,2,1)
